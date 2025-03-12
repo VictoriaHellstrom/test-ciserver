8e0c411ca95ead0f1d0915ebf20462ad9ffa38f2
VictoriaHellstrom <victoria@hellstrom.email>
2025-03-12 15:45:41 +0100

    hffdgfd

--- src/main/java/com/exempel/App.java
+++ src/main/java/com/exempel/App.java
@@ -6,6 +6,6 @@ package com.exempel;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World! kjsdfnsd");
+        System.out.println("Hello World! hffdgf");
     }
 }
